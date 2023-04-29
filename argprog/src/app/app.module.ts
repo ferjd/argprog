5d41a5afa3e673184e7124a3bec25a3ccb057854
ferjd <ferchomails@gmail.com>
2023-04-29 20:49:09 -0300

    header

--- argprog/src/app/app.module.ts
+++ argprog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comp/header/header.component';
+import { AplogoComponent } from './comp/aplogo/aplogo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AplogoComponent
   ],
   imports: [
     BrowserModule,
