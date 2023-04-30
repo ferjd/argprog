d11e0043207cea017757b08d1230ea6badba922d
ferjd <ferchomails@gmail.com>
2023-04-30 01:29:59 -0300

    hard & soft Skills

--- argprog/src/app/app.module.ts
+++ argprog/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Import ng-circle-progress*/
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './comp/header/header.component';
@@ -10,6 +13,7 @@ import { BannerComponent } from './comp/banner/banner.component';
 import { AcercaDeComponent } from './comp/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './comp/experiencia/experiencia.component';
 import { EducacionComponent } from './comp/educacion/educacion.component';
+import { HardsoftComponent } from './comp/hardsoft/hardsoft.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +24,20 @@ import { EducacionComponent } from './comp/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HardsoftComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
