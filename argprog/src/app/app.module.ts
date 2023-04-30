927a0e2c4802ce162f5b66d395a520fce4231fda
ferjd <ferchomails@gmail.com>
2023-04-29 21:27:56 -0300

    banner
    header y banner provisional

--- argprog/src/app/app.module.ts
+++ argprog/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './comp/header/header.component';
 import { AplogoComponent } from './comp/aplogo/aplogo.component';
+import { SocialComponent } from './comp/social/social.component';
+import { BannerComponent } from './comp/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AplogoComponent
+    AplogoComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
